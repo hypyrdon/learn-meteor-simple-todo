c3fbe01bd52a95971b03ab8a843a444faa3345bd
Hypyrdon <hypyrdon.tmpgit@hypyrdon.com>
2015-03-18 19:47:50 -0400

    finished part tut3

--- simple-todos.js
+++ simple-todos.js
@@ -1,19 +1,21 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  Template.body.helpers({
+      tasks: function () {
+        return Tasks.find({});
+      }
   });
 
+  /*
   Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+  */
 }
 
 if (Meteor.isServer) {
