18dd145c5c693e2fa2440a39c9c6988dededb20f
Hypyrdon <hypyrdon.tmpgit@hypyrdon.com>
2015-04-06 18:10:06 -0400

    chap04 working

--- simple-todos.js
+++ simple-todos.js
@@ -4,18 +4,21 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   Template.body.helpers({
       tasks: function () {
-        return Tasks.find({});
+        return Tasks.find({}, {sort: {createdAt:-1}});
       }
   });
 
-  /*
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.events({
+    'submit .new-task': function (event) {
+      var textOfNewTodo = event.target.text.value;
+      Tasks.insert({
+        text: textOfNewTodo,
+        createdAt: new Date()
+      });
+      event.target.text.value = "";
+      return false; //prevent form submit
     }
   });
-  */
 }
 
 if (Meteor.isServer) {
